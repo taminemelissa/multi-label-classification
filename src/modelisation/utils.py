8d9f5797f5752d81d1a8821b3478f514c13168e0
Tamine Mélissa <melissa.tamine@ensae.fr>
2022-02-08 11:43:08 +0100

    reformat utils.py

--- src/modelisation/utils.py
+++ src/modelisation/utils.py
@@ -8,44 +8,45 @@ def create_labels(data, n_labels):
                 labels[i].append(labels_names[j])
     return labels
 
+
 def transform_labels_into_strings(L):
     string = ''
     for elt in L:
-        if len(string)==0:
-            string+=elt
-        else :
-            string+='/'+elt
+        if len(string) == 0:
+            string += elt
+        else:
+            string += '/' + elt
     return string
 
-def count_label_occurences(L): 
-   
+
+def count_label_occurences(L):
     count = 0
     values = []
-    freq = {} 
-  
-    for x in L: 
+    freq = {}
+
+    for x in L:
         label = transform_labels_into_strings(x)
-        if (x in values): 
+        if x in values:
             freq[label] += 1
-        else: 
+        else:
             freq[label] = 1
             values.append(x)
-  
-    
-    for key, value in freq.items(): 
-        if value == 1: 
+
+    for key, value in freq.items():
+        if value == 1:
             count += 1
-  
+
     return count, freq
+
 
 def keep_unique_label_data(df):
     df_unique = df
     to_drop = []
     labels = create_labels(df_unique, 21)
-    for i in range(len(labels)) :
-        if len(labels[i])>1:
+    for i in range(len(labels)):
+        if len(labels[i]) > 1:
             to_drop.append(i)
-    df_unique.drop(to_drop,0,inplace=True)
+    df_unique.drop(to_drop, 0, inplace=True)
     return df_unique
 
 
@@ -53,4 +54,3 @@ def convert_string_labels_into_numbers(df, labels):
     genres = list(df.columns[:21])
     Y = [genres.index(label[0]) for label in labels]
     return Y
-
