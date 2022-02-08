5094b30f9424f5a0383c425f83153b5b4c102b87
mtamine <melissa.tamine@ensae.fr>
2022-02-08 08:44:00 +0000

    modify utils + create notebooks

--- src/modelisation/utils.py
+++ src/modelisation/utils.py
@@ -38,3 +38,19 @@ def count_label_occurences(L):
   
     return count, freq
 
+def keep_unique_label_data(df):
+    df_unique = df
+    to_drop = []
+    labels = create_labels(df_unique, 21)
+    for i in range(len(labels)) :
+        if len(labels[i])>1:
+            to_drop.append(i)
+    df_unique.drop(to_drop,0,inplace=True)
+    return df_unique
+
+
+def convert_string_labels_into_numbers(df, labels):
+    genres = list(df.columns[:21])
+    Y = [genres.index(label[0]) for label in labels]
+    return Y
+
