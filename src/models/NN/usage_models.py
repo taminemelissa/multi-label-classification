5831ab54687a3a4671ee77b66d15d42e2b032b7f
mtamine <melissa.tamine@ensae.fr>
2022-05-11 09:11:15 +0000

    add new models

--- src/models/NN/usage_models.py
+++ src/models/NN/usage_models.py
@@ -30,3 +30,29 @@ class UsageNet2(BaseNet):
         output = self.dropout(output)
         output = self.fc3(output)
         return output
+
+class UsageNet3(BaseNet):  
+
+    def __init__(self):
+        super(UsageNet3,self).__init__()
+        self.data_type = 1
+        self.dropout = nn.Dropout(0.3)
+        self.fc1 = nn.Linear(128, 104)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(104,52)
+        self.fc3 = nn.Linear(52,21)
+        self.norm1 = nn.BatchNorm1d(104)
+        self.norm2 = nn.BatchNorm1d(52)
+        self.norm3 = nn.BatchNorm1d(21)
+    
+    def forward(self, x):
+        output = self.dropout(x)
+        output = self.fc1(output)
+        output = self.norm1(output)
+        output = self.relu(output)
+        output = self.fc2(output)
+        output = self.norm2(output)
+        output = self.relu(output)
+        output = self.fc3(output)
+        output = self.norm3(output)
+        return output
